35eca4700904644903b83204da9f6495f89847e8
tomishima <pumoco2904@icloud.com>
2023-03-29 20:59:58 +0900

    Sprinb-boot: gradleでプロジェクト作成

---
+++ src/main/java/chibainfo5/es_manager/EsManagerApplication.java
@@ -0,0 +1,13 @@
+package chibainfo5.es_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chibainfo5/es_manager/EsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package chibainfo5.es_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
