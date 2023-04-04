d13ee3a0edd4e581060f619863012fca1e5d19d8
tomishima <pumoco2904@icloud.com>
2023-04-04 17:11:17 +0900

    Hello World完成 #3

--- src/main/java/chibainfo5/es_manager/HelloController.java
+++ src/main/java/chibainfo5/es_manager/HelloController.java
@@ -2,13 +2,13 @@ package chibainfo5.es_manager;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping("/")
+
 @Controller
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/")
     public String hello() {
         return "index";
     }
