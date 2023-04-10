2f083d30396ba246162d90bd663b0c25374274bf
mctomoney <mctomoney0701@gmail.com>
2023-04-10 23:13:34 +0900

    Repositoryファイルを作成（未完成）

---
+++ backend/src/main/java/chibainfo5/es_manager/repository/EsRepository.java
@@ -0,0 +1,6 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface EsRepository extends JpaRepository<EsEntity, Integer> {
+    // 必要なメソッドを追加予定（勉強中）
+}
