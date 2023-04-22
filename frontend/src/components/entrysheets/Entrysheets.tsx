74000df584454871026550bc728f0234e5704c26
tomishima <pumoco2904@icloud.com>
2023-04-22 21:57:18 +0900

    entrysheetsコンポーネントのスタイルを作成

--- frontend/src/components/entrysheets/Entrysheets.tsx
+++ frontend/src/components/entrysheets/Entrysheets.tsx
@@ -3,6 +3,20 @@ import {
   EntrysheetsProps,
 } from "@/types/LightEntrysheetProps";
 
+// ヘッダー行
+const Header = () => {
+  return (
+    <div className="flex items-center px-4 py-2 border-b border-gray-300">
+      <div className="font-bold flex-grow flex-shrink-0">企業</div>
+      <div className="font-bold w-1/4 flex-shrink-0">職種</div>
+      <div className="font-bold w-1/4 flex-shrink-0">イベント</div>
+      <div className="font-bold w-1/4 flex-shrink-0">
+        <button className="focus:outline-none">締切</button>
+      </div>
+    </div>
+  );
+};
+
 // EntrySheetsでリスト表示する際のentrysheet1行
 const RowOfEntrysheet = (props: {
   entrysheet: LightEntrysheetProps;
@@ -21,49 +35,32 @@ const RowOfEntrysheet = (props: {
     .padStart(2, "0")}:${deadline.getMinutes().toString().padStart(2, "0")}`;
 
   return (
-    <tr
-      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700
-    hover:bg-gray-50 dark:hover:bg-gray-600"
+    <li
+      className="flex items-center px-4 py-2 border-b border-gray-300
+    transition duration-300 ease-in-out hover:bg-gray-100"
     >
-      <td className="px-6 py-4">{entrysheet.company}</td>
-      <td className="px-6 py-4">{entrysheet.job}</td>
-      <td className="px-6 py-4">{entrysheet.event}</td>
-      <td className="px-6 py-4">{formattedDate}</td>
-    </tr>
+      <div className="flex-grow flex-shrink-0">{entrysheet.company}</div>
+      <div className="w-1/4 flex-shrink-0">{entrysheet.job}</div>
+      <div className="w-1/4 flex-shrink-0">{entrysheet.event}</div>
+      <div className="w-1/4 flex-shrink-0">{formattedDate}</div>
+    </li>
   );
 };
 
-// EntrySheetsリストを表示
+// EntrySheetsリストのコンポーネント
 const ListOfEntrysheets = (props: {
   entrysheets: EntrysheetsProps;
 }): JSX.Element => {
   const { entrysheets } = props;
 
   return (
-    <div className="relative overflow-x-auto">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              企業
-            </th>
-            <th scope="col" className="px-6 py-3">
-              職種
-            </th>
-            <th scope="col" className="px-6 py-3">
-              イベント
-            </th>
-            <th scope="col" className="px-6 py-3">
-              締切
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(entrysheets).map((esId) => (
-            <RowOfEntrysheet key={esId} entrysheet={entrysheets[esId]} />
-          ))}
-        </tbody>
-      </table>
+    <div className="rounded-lg bg-white">
+      <Header />
+      <ul className="list-none">
+        {Object.keys(entrysheets).map((esId) => (
+          <RowOfEntrysheet key={esId} entrysheet={entrysheets[esId]} />
+        ))}
+      </ul>
     </div>
   );
 };
