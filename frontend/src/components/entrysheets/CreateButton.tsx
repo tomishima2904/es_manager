b94d05c2b1f4a273873ddb4aa4e3efe381bc9ccc
tomishima <pumoco2904@icloud.com>
2023-04-24 19:42:25 +0900

    createNewEntrysheet関数をコンポーネント内部で定義

--- frontend/src/components/entrysheets/CreateButton.tsx
+++ frontend/src/components/entrysheets/CreateButton.tsx
@@ -1,36 +1,38 @@
 import axios from "axios";
-import { useRouter } from "next/router";
+import type { NextRouter } from "next/router";
 import { IconContext } from "react-icons";
 import { AiOutlineFileAdd } from "react-icons/ai"; // 画像icon
 
-const createNewEntrysheet = async () => {
-  const ENDPOINT: string = "/api/user/entrysheets";
-  try {
-    const response = await axios.post(
-      ENDPOINT,
-      {},
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    console.log("Success", response);
-    // レスポンスオブジェクトから新しく作成されたリソースのIDを取得
-    const esID: string = response.data.esID.toString();
-    const newENDPOINT: string = "user/entrysheets/" + esID;
-    // useRouterでnewENDPOINTへ画面遷移
-    const router = useRouter();
-    router.push({ pathname: newENDPOINT });
-  } catch (error) {
-    console.error("Error", error);
-  }
-};
+export default function CreateButton(props: { router: NextRouter }) {
+  // POSTメソッドで新しいエントリーシートを作成
+  const createNewEntrysheet = async () => {
+    const ENDPOINT: string = "/api/user/entrysheets";
+    try {
+      const response = await axios.post(
+        ENDPOINT,
+        {},
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log("Success", response);
+      // レスポンスオブジェクトから新しく作成されたリソースのIDを取得
+      const esId: string = response.data.esId.toString();
+      const newENDPOINT: string = "entrysheets/" + esId;
+      // useRouterでnewENDPOINTへ画面遷移
+      const { router } = props;
+      router.push({ pathname: newENDPOINT });
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
 
-export default function CreateButton() {
   return (
     <IconContext.Provider value={{ className: "w-6 h-6" }}>
       <button
         className="flex flex-shrink-0 justify-center items-center
         rounded-lg border shadow-2xl drop-shadow-md
+        hover:bg-gray-100 transition duration-300 ease-in-out
         text-lg px-4 py-2 h-10"
         onClick={createNewEntrysheet}
       >
