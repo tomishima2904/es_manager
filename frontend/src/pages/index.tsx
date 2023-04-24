c29aa2e341c0375a6ecf36658035b6df7e3a8902
tomishima <pumoco2904@icloud.com>
2023-04-24 19:43:45 +0900

    挙動確認ように仮のリソースを作成

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,8 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import ListOfEntrysheets from "@/components/entrysheets/Entrysheets";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <>
-      <ListOfEntrysheets />
-    </>
-  );
+  return <div>hello</div>;
 }
