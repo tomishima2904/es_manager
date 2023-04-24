67ffebb58742271cff4ab056d1ada80546b89420
tomishima <pumoco2904@icloud.com>
2023-04-24 09:03:27 +0900

    Header comp.の名称変更 & comp.を丸角にしてpadding

--- frontend/src/components/entrysheets/EntrysheetsList.tsx
+++ frontend/src/components/entrysheets/EntrysheetsList.tsx
@@ -4,9 +4,9 @@ import {
 } from "@/types/LightEntrysheetProps";
 
 // ヘッダー行
-const Header = () => {
+const ListHeader = () => {
   return (
-    <div className="flex items-center px-4 py-2 border-b border-gray-300">
+    <div className="flex items-center px-4 py-2 border-b border-gray-300 bg-white">
       <div className="font-bold flex-grow flex-shrink-0">企業</div>
       <div className="font-bold w-1/4 flex-shrink-0">職種</div>
       <div className="font-bold w-1/4 flex-shrink-0">イベント</div>
@@ -36,7 +36,7 @@ const EntrysheetItem = (props: {
 
   return (
     <li
-      className="flex items-center px-4 py-2 border-b border-gray-300
+      className="flex items-center px-4 py-2 border-b border-gray-300 bg-white
     transition duration-300 ease-in-out hover:bg-gray-100"
     >
       <div className="flex-grow flex-shrink-0">{entrysheet.company}</div>
@@ -54,8 +54,8 @@ const EntrysheetsList = (props: {
   const { entrysheets } = props;
 
   return (
-    <main className="flex-1 rounded-lg bg-white">
-      <Header />
+    <main className="flex-1 bg-white p-4 rounded-lg">
+      <ListHeader />
       <ul className="list-none">
         {Object.keys(entrysheets).map((esId) => (
           <EntrysheetItem key={esId} entrysheet={entrysheets[esId]} />
