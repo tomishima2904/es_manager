e111e6c17107688af2081f81e2ee685bb4ec1ce8
tomishima <pumoco2904@icloud.com>
2023-04-24 19:41:23 +0900

    user/entrysheetsのAPIに対してPOSTメソッドを定義

--- frontend/src/pages/api/user/entrysheets.ts
+++ frontend/src/pages/api/user/entrysheets.ts
@@ -2,27 +2,35 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type {
   LightEntrysheetProps,
   EntrysheetsProps,
-} from "@/types/LightEntrysheetProps";
+  NewEntrysheetProps,
+} from "@/types/EntrysheetProps";
 
 export default (
   req: NextApiRequest,
-  res: NextApiResponse<EntrysheetsProps>
+  res: NextApiResponse<EntrysheetsProps | NewEntrysheetProps>
 ) => {
-  const entrysheet1: LightEntrysheetProps = {
-    company: "A株式会社",
-    job: "総合職",
-    event: "夏インターン",
-    deadline: "2023-03-31T12:00:00",
-  };
-  const entrysheet2: LightEntrysheetProps = {
-    company: "株式会社B",
-    job: "エンジニア",
-    event: "本選考",
-    deadline: "2023-04-21T18:00:00",
-  };
-  const entrysheets: { [key: string]: LightEntrysheetProps } = {
-    "0": entrysheet1,
-    "1": entrysheet2,
-  };
-  res.status(200).json(entrysheets);
+  if (req.method === "POST") {
+    const entrysheet: NewEntrysheetProps = {
+      esId: "0",
+    };
+    res.status(200).json(entrysheet);
+  } else {
+    const entrysheet1: LightEntrysheetProps = {
+      company: "A株式会社",
+      job: "総合職",
+      event: "夏インターン",
+      deadline: "2023-03-31T12:00:00",
+    };
+    const entrysheet2: LightEntrysheetProps = {
+      company: "株式会社B",
+      job: "エンジニア",
+      event: "本選考",
+      deadline: "2023-04-21T18:00:00",
+    };
+    const entrysheets: { [key: string]: LightEntrysheetProps } = {
+      "0": entrysheet1,
+      "1": entrysheet2,
+    };
+    res.status(200).json(entrysheets);
+  }
 };
