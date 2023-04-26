742baa1bf6e43507c3eaa4dc58ced8778e7a5840
tomishima <pumoco2904@icloud.com>
2023-04-26 20:54:17 +0900

    `GET user/entrysheet/[esId]`のresを受け取れるようにした

--- frontend/src/pages/user/entrysheets/[esId].tsx
+++ frontend/src/pages/user/entrysheets/[esId].tsx
@@ -1,10 +1,7 @@
 import { GetServerSidePropsContext } from "next";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import EditingEntrysheet from "@/components/editing/EditingEntrysheet";
-
-const Entrysheet = ({ query }: GetServerSidePropsContext) => {
-  console.log(query.esId);
-  return <EditingEntrysheet />;
-};
 
 // 動的なパスの値を取得
 export async function getServerSideProps({ query }: GetServerSidePropsContext) {
@@ -12,5 +9,19 @@ export async function getServerSideProps({ query }: GetServerSidePropsContext) {
     props: { query },
   };
 }
+
+const Entrysheet = ({ query }: GetServerSidePropsContext) => {
+  const [entrysheet, setEntrysheet] = useState({});
+  useEffect(() => {
+    const getEntrysheets = async () => {
+      const ENDPOINT: string = "/api/user/entrysheets/" + query.esId;
+      const result = await axios.get(ENDPOINT).then((res) => res.data);
+      setEntrysheet(result);
+    };
+    getEntrysheets();
+  }, []);
+
+  return <EditingEntrysheet />;
+};
 
 export default Entrysheet;
