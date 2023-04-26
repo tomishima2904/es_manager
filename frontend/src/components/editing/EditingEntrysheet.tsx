47212affcfaf98b759bea8ef181e81839af76331
tomishima <pumoco2904@icloud.com>
2023-04-26 21:22:50 +0900

    `GET`のレスポンスデータをコンポーネントへ渡せるようにした

--- frontend/src/components/editing/EditingEntrysheet.tsx
+++ frontend/src/components/editing/EditingEntrysheet.tsx
@@ -1,5 +1,10 @@
-const EditingEntrysheet = () => {
-  return <div className="p-1">This is a dummy entrysheet</div>;
+import { RichEntrysheetProps } from "@/types/EntrysheetProps";
+
+const EditingEntrysheet = (props: {
+  entrysheet: RichEntrysheetProps;
+}): JSX.Element => {
+  const { esId, company, job, event, deadline, questions } = props.entrysheet;
+  return <div className="p-1">This is {esId} entrysheet</div>;
 };
 
 export default EditingEntrysheet;
--- frontend/src/pages/user/entrysheets/[esId].tsx
+++ frontend/src/pages/user/entrysheets/[esId].tsx
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext } from "next";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import EditingEntrysheet from "@/components/editing/EditingEntrysheet";
+import { RichEntrysheetProps } from "@/types/EntrysheetProps";
 
 // 動的なパスの値を取得
 export async function getServerSideProps({ query }: GetServerSidePropsContext) {
@@ -11,17 +12,24 @@ export async function getServerSideProps({ query }: GetServerSidePropsContext) {
 }
 
 const Entrysheet = ({ query }: GetServerSidePropsContext) => {
-  const [entrysheet, setEntrysheet] = useState({});
+  const [entrysheet, setEntrysheet] = useState<RichEntrysheetProps>({
+    esId: "",
+    company: "",
+    job: "",
+    event: "",
+    deadline: "",
+    questions: {},
+  });
   useEffect(() => {
     const getEntrysheets = async () => {
       const ENDPOINT: string = "/api/user/entrysheets/" + query.esId;
       const result = await axios.get(ENDPOINT).then((res) => res.data);
-      setEntrysheet(result);
+      setEntrysheet((prevEntrysheet) => ({ ...prevEntrysheet, ...result }));
     };
     getEntrysheets();
   }, []);
 
-  return <EditingEntrysheet />;
+  return <EditingEntrysheet entrysheet={entrysheet} />;
 };
 
 export default Entrysheet;
