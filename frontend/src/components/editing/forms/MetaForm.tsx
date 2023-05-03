1d937605d7b1f7b44cdd2393d3a673a553efa358
tomishima <pumoco2904@icloud.com>
2023-05-03 22:49:40 +0900

    refactor: #15 DoubleClickFormを使ってメタデータを書き換え

--- frontend/src/components/editing/forms/MetaForm.tsx
+++ frontend/src/components/editing/forms/MetaForm.tsx
@@ -1,24 +1,44 @@
-const JobForm = (props: { job: string | null }): JSX.Element => {
+import DoubleClickForm from "./DoubleClickForm";
+
+const classNames: string[] = [
+  "px-2 py-1 rounded-lg border border-gray-300 bg-transparent",
+  "",
+];
+const wrapper: string = "mr-8 pr-8 flex justify-around items-center";
+const borderWrapper: string = wrapper + " border-r border-gray-300";
+const spanClassName: string = "mr-3 font-bold ";
+
+const JobForm = (props: { job: string }): JSX.Element => {
   const { job } = props;
-  return <div className="border-r border-gray-300 mr-8 pr-8">職種: {job}</div>;
+  return (
+    <div className={borderWrapper}>
+      <span className={spanClassName}>職種:</span>
+      <DoubleClickForm text={job} classNames={classNames} />
+    </div>
+  );
 };
 
-const EventForm = (props: { event: string | null }): JSX.Element => {
+const EventForm = (props: { event: string }): JSX.Element => {
   const { event } = props;
   return (
-    <div className="border-r border-gray-300 mr-8 pr-8">イベント: {event}</div>
+    <div className={borderWrapper}>
+      <span className={spanClassName}>イベント:</span>
+      <DoubleClickForm text={event} classNames={classNames} />
+    </div>
   );
 };
 
 const DeadlineForm = (): JSX.Element => {
-  return <div>締切: hoge</div>;
+  return (
+    <div className={wrapper}>
+      <span className={spanClassName}>締切:</span>
+      <DoubleClickForm text={"ほげ"} classNames={classNames} />
+    </div>
+  );
 };
 
 // job と event は null でも良い
-const MetaForm = (props: {
-  job: string | null;
-  event: string | null;
-}): JSX.Element => {
+const MetaForm = (props: { job: string; event: string }): JSX.Element => {
   const { job, event } = props;
   return (
     <div className="flex justify-start items-center p-2">
