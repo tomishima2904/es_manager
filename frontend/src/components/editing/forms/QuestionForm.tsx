437a75b4900633ad6ed3efa680fd56bb34632c9e
tomishima <pumoco2904@icloud.com>
2023-05-05 21:43:23 +0900

    feat: #14 質問フォームもダブルクリックで編集できるようにした

--- frontend/src/components/editing/forms/QuestionForm.tsx
+++ frontend/src/components/editing/forms/QuestionForm.tsx
@@ -3,43 +3,46 @@ import validateInput from "@/utils/validateInputError";
 import EditButton from "../buttons/EditButton";
 import FormWithError from "./FormWithError";
 
-const limitChars = 100;
-
-// 編集中でなければテキストを表示
-const TextDisplay = (props: { text: string }): JSX.Element => {
-  return <p>{props.text}</p>;
-};
-
 const QuestionForm = (props: { question: string }): JSX.Element => {
-  const { question } = props;
-  const [text, setText] = useState<string>(question);
-  const [error, setError] = useState("");
-  // フォームの変更を検知
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
-    const inputValue = e.target.value;
-    setText(inputValue);
-    validateInput(inputValue, limitChars, setError);
-  };
-  // 編集可能にするかを切り替える
+  const [text, setText] = useState<string>(props.question);
   const [isEditing, setIsEditing] = useState<boolean>(false);
-  const handleEditButtonClick = (): void => setIsEditing((prev) => !prev);
+  const [tempText, setTempText] = useState<string>("");
+  const limitChars = 100; // DBで格納できる最大の文字長
+
+  const startEditing = () => {
+    setTempText(text);
+    setIsEditing(true);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTempText(e.target.value);
+  };
+
+  // フォーカスが外れたら編集完了
+  const handleBlur = () => {
+    setText(tempText);
+    setIsEditing(false);
+  };
 
   return (
-    <div>
-      {isEditing ? (
+    <>
+      {isEditing || text.trim() === "" ? (
         <div className="flex items-center">
-          <FormWithError
-            text={text}
-            handleChange={handleChange}
-            error={error}
+          <textarea
+            className="my-form"
+            value={tempText}
+            onChange={handleChange}
+            onBlur={handleBlur}
             placeholder="例. あなたの強みを教えてください。"
+            autoFocus
           />
         </div>
       ) : (
-        <TextDisplay text={text} />
+        <div className="" onDoubleClick={startEditing}>
+          {text}
+        </div>
       )}
-      <EditButton isEditing={isEditing} onClick={handleEditButtonClick} />
-    </div>
+    </>
   );
 };
 
