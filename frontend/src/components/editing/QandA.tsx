62056dbf62b3ef7a85e400346aa240a2b416719c
tomishima <pumoco2904@icloud.com>
2023-05-05 22:30:56 +0900

    feat: #16 SetCharsFormを追加（機能未実装）

--- frontend/src/components/editing/QandA.tsx
+++ frontend/src/components/editing/QandA.tsx
@@ -1,4 +1,5 @@
 import QuestionForm from "./forms/QuestionForm";
+import SetCharsForm from "./forms/SetCharsForm";
 import AnswerForms from "./forms/AnswerForms";
 import type { QandAProps } from "@/types/EntrysheetProps";
 
@@ -6,8 +7,15 @@ const QandA = (props: { qAndAProps: QandAProps }) => {
   const { question, maxChars, answers } = props.qAndAProps;
 
   return (
-    <form className="p-4 flex-grow border-b border-gray-300">
-      <QuestionForm question={question} />
+    <form className="p-4 flex-grow justify-between border-b border-gray-300">
+      <div className="flex w-full">
+        <div className="w-11/12">
+          <QuestionForm question={question} />
+        </div>
+        <div className="w-1/12 ml-2">
+          <SetCharsForm maxChars={maxChars} />
+        </div>
+      </div>
       <AnswerForms answers={answers} maxChars={maxChars} />
     </form>
   );
---
+++ frontend/src/components/editing/forms/SetCharsForm.tsx
@@ -0,0 +1,5 @@
+const SetCharsForm = (props: { maxChars: number }): JSX.Element => {
+  return <input type="number" maxLength={4} className="my-number-form" />;
+};
+
+export default SetCharsForm;
