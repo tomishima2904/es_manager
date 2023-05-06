eb973dc64e92305b5a3435c0b426a23eca06b86e
tomishima <pumoco2904@icloud.com>
2023-05-06 21:23:39 +0900

    feat: #16 isOverがtrueならmy-form-warningスタイルを適用

--- frontend/src/components/editing/forms/FormWithError.tsx
+++ frontend/src/components/editing/forms/FormWithError.tsx
@@ -1,14 +1,15 @@
 const FormWithError = (props: {
   text: string;
   handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  isOver: boolean;
   error: string;
   placeholder: string;
 }): JSX.Element => {
-  const { text, handleChange, error, placeholder } = props;
+  const { text, handleChange, isOver, error, placeholder } = props;
   return (
     <>
       <textarea
-        className="my-form"
+        className={isOver ? "my-form-warning" : "my-form"}
         value={text}
         onChange={handleChange}
         placeholder={placeholder}
