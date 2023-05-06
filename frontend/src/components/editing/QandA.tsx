0f212ebadcadc8d1d1b9066c6847f8b3c0374ab5
tomishima <pumoco2904@icloud.com>
2023-05-06 21:25:40 +0900

    feat: #16 maxCharsを各comp.で扱うために親であるQandAを改変

--- frontend/src/components/editing/QandA.tsx
+++ frontend/src/components/editing/QandA.tsx
@@ -1,10 +1,13 @@
 import QuestionForm from "./forms/QuestionForm";
 import SetCharsForm from "./forms/SetCharsForm";
 import AnswerForms from "./forms/AnswerForms";
-import type { QandAProps } from "@/types/EntrysheetProps";
+import type { QandAProps, AnswersProps } from "@/types/EntrysheetProps";
+import { useState } from "react";
 
 const QandA = (props: { qAndAProps: QandAProps }) => {
-  const { question, maxChars, answers } = props.qAndAProps;
+  const question: string = props.qAndAProps.question;
+  const answers: AnswersProps = props.qAndAProps.answers;
+  const [maxChars, setMaxChars] = useState<number>(props.qAndAProps.maxChars);
 
   return (
     <form className="p-4 flex-grow justify-between border-b border-gray-300">
@@ -13,7 +16,7 @@ const QandA = (props: { qAndAProps: QandAProps }) => {
           <QuestionForm question={question} />
         </div>
         <div className="w-1/12 ml-2">
-          <SetCharsForm maxChars={maxChars} />
+          <SetCharsForm maxChars={maxChars} setMaxChars={setMaxChars} />
         </div>
       </div>
       <AnswerForms answers={answers} maxChars={maxChars} />
