3a2c2eb682ed5bfcba3ae28d15eb56e13f7db0d6
mctomoney <mctomoney0701@gmail.com>
2023-05-09 14:34:49 +0900

    少しクラスメソッド追加してみた

--- backend/src/main/java/chibainfo5/es_manager/repository/EsRepository.java
+++ backend/src/main/java/chibainfo5/es_manager/repository/EsRepository.java
@@ -3,4 +3,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EsRepository extends JpaRepository<EsEntity, Integer> {
     // 必要なメソッドを追加予定（勉強中）
+    List<EsEntity> findByFirstName(Int esId);
 }
