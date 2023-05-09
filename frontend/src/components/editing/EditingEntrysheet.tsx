5c4dec5a01976ec503e2517863066ae93ee4d9a9
tomishima <pumoco2904@icloud.com>
2023-05-09 14:08:16 +0900

    refactor: #21 nullを受け取らずに初期化するようにした

--- frontend/src/components/editing/EditingEntrysheet.tsx
+++ frontend/src/components/editing/EditingEntrysheet.tsx
@@ -11,16 +11,12 @@ const EditingEntrysheet = (props: {
   entrysheet: RichEntrysheetProps;
 }): JSX.Element => {
   const { esId, company, job, event, deadline, questions } = props.entrysheet;
-  // コンポーネントが最初にレンダリングされたときに、props.entrysheetの値が設定されない
-  // そこで遅延初期化を行うため、nullを入れておく
-  const [qAndAs, setQandAs] = useState<QuestionsProps | null>(null);
+  const [qAndAs, setQandAs] = useState<QuestionsProps>({});
 
-  // レンダリング時にprops.entrysheetが存在する場合は、初期化を実行する
+  // questionsの値によってレンダリングを再実行
   useEffect(() => {
-    if (props.entrysheet) {
-      setQandAs({ ...props.entrysheet.questions });
-    }
-  }, [props.entrysheet]);
+    setQandAs({ ...questions });
+  }, [questions]);
 
   if (!qAndAs) {
     return <div>Loading...</div>;
