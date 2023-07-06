454676ad054018f13b7202d0d52b39c1767829f8
tomishima <pumoco2904@icloud.com>
2023-07-06 20:25:13 +0900

    feat: #32 公式通りに認証を実装 (loginに勝手にredirectされる)

---
+++ backend/src/main/java/chibainfo5/es_manager/securingweb/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
