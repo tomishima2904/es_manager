ae19be8fcac13b63c66b51ac7a5da2c5a8218fb1
tomishima <pumoco2904@icloud.com>
2023-07-12 22:35:56 +0900

    feat: #33 indexはオブジェクトを返す必要がないのでStringに修正

--- backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
+++ backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
@@ -3,14 +3,12 @@ package chibainfo5.es_manager.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 
 @RestController
 public class HelloController {
-    // FIXME: index関数の型の定義がおそらくスタンダードじゃない気がする
-    @GetMapping("/")  // URLを指定
-    public Map<String, String> index() {
-        return Map.of("msg", "Hello!");  // JSON (Pythonでいえばdict型のオブジェクトを返す)
+
+    @GetMapping("/")  // 指定したURLに対してGETメソッドを行う
+    public String index() {
+        return "Hello! Here is index";
     }
 }
