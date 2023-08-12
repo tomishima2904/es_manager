5f74a415827e067e5e5677e13573c27c34b2172b
tomishima <pumoco2904@icloud.com>
2023-08-12 20:31:19 +0900

    feat: #33 リポジトリを使ってDBからユーザーIDでデータ取得可能に

--- backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
+++ backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
@@ -1,7 +1,9 @@
 package chibainfo5.es_manager.controllers;
 
 import chibainfo5.es_manager.entity.EntrysheetsEntity;
+import chibainfo5.es_manager.repositories.EntrysheetsRepository;
 
+// For WebFlux
 import reactor.core.publisher.Mono;
 import javax.annotation.PostConstruct;
 
@@ -10,12 +12,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+// For Repository
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class HelloController {
+
+    // データベースアクセスのために必要なリポジトリを定義
+    @Autowired
+    private EntrysheetsRepository entrysheetsRepository;
+
     // アプリ起動時にダミーデータをデータベース内に登録
     @PostConstruct
     public void init(){
@@ -33,16 +43,12 @@ public class HelloController {
         return Mono.just("Hello Spring!");
     }
 
+    // ユーザー毎のエントリーシートリストのページ
+    // 任意のユーザーIDのエントリーシート情報を取得する
+    // TODO: 表示URLを `/me`のようにダミーにする
     @GetMapping("/{userId}")
     public Mono<List<EntrysheetsEntity>> getUserEntrysheets(@PathVariable Long userId) {
-        List<EntrysheetsEntity> entrysheets = new ArrayList<>();
-
-        EntrysheetsEntity entrysheet1 = new EntrysheetsEntity(userId, 0L, "A株式会社", "エンジニア", null, "本選考", false);
-        entrysheets.add(entrysheet1);
-
-        EntrysheetsEntity entrysheet2 = new EntrysheetsEntity(userId, 1L, "B株式会社", "総合職", null, "夏インターン", true);
-        entrysheets.add(entrysheet2);
-
+        List<EntrysheetsEntity> entrysheets = entrysheetsRepository.findByUserId(userId);
         return Mono.just(entrysheets);
     }
 }
--- backend/src/main/java/chibainfo5/es_manager/repository/EntrysheetsRepository.java
+++ backend/src/main/java/chibainfo5/es_manager/repository/EntrysheetsRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import chibainfo5.es_manager.entity.EntrysheetsEntity;
+import chibainfo5.es_manager.entity.EntrysheetsKey;
+
+import java.util.List;
+
 
 @Repository
-public interface EntrysheetsRepository extends JpaRepository<EntrysheetsEntity, Long> {
+public interface EntrysheetsRepository extends JpaRepository<EntrysheetsEntity, EntrysheetsKey> {
+    public List<EntrysheetsEntity> findByUserId(Long userId);
 }
