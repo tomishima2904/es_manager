727385918f432d7e9fb9d3e0c5112a892ab19dde
tomishima <pumoco2904@icloud.com>
2023-08-12 20:29:12 +0900

    feat: #33 PostConstructでapp起動時にDBを初期化

--- backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
+++ backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
@@ -13,6 +13,18 @@ import java.util.List;
 
 @RestController
 public class HelloController {
+    // アプリ起動時にダミーデータをデータベース内に登録
+    @PostConstruct
+    public void init(){
+        EntrysheetsEntity entrysheet1 = new EntrysheetsEntity(0L, 0L, "A株式会社", "エンジニア", null, "本選考", false);
+        EntrysheetsEntity entrysheet2 = new EntrysheetsEntity(1L, 0L, "C株式会社", "データサイエンティスト", null, "冬インターン", false);
+        EntrysheetsEntity entrysheet3 = new EntrysheetsEntity(0L, 1L, "B株式会社", "総合職", null, "夏インターン", true);
+        entrysheetsRepository.saveAndFlush(entrysheet1);
+        entrysheetsRepository.saveAndFlush(entrysheet2);
+        entrysheetsRepository.saveAndFlush(entrysheet3);
+    }
+
+    // トップページ
     @GetMapping("/")  // 指定したURLに対してGETメソッドを行う
     public Mono<String> index() {
         return Mono.just("Hello Spring!");
