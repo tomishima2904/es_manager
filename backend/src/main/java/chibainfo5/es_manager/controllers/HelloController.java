36fcff8478609e23ec9a054511ddb9c2a4ce5d82
tomishima <pumoco2904@icloud.com>
2023-08-12 16:09:28 +0900

    feat: #33 webfluxで簡易なRestAPIの記述

--- backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
+++ backend/src/main/java/chibainfo5/es_manager/controllers/HelloController.java
@@ -1,14 +1,14 @@
 package chibainfo5.es_manager.controllers;
 
+import reactor.core.publisher.Mono;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
-
     @GetMapping("/")  // 指定したURLに対してGETメソッドを行う
-    public String index() {
-        return "Hello! Here is index";
+    public Mono<String> index() {
+        return Mono.just("Hello Spring!");
     }
 }
