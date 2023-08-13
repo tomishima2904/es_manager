1032f58481859b492bb9a92aad1c5effcf1ec00b
tomishima <pumoco2904@icloud.com>
2023-08-13 20:47:44 +0900

    refactor: #33 MYSQLのホストを環境変数で指定, 不要なパッケージを削除

--- backend/src/main/java/chibainfo5/es_manager/domain/QuestionsResponse.java
+++ backend/src/main/java/chibainfo5/es_manager/domain/QuestionsResponse.java
@@ -6,7 +6,6 @@ import chibainfo5.es_manager.domain.QuestionsEntity;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-import org.json.JSONObject;
 
 
 public class QuestionsResponse {
