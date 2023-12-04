28ec7f4b546fabafcfdc76157b16514855ab6860
tomishima <pumoco2904@icloud.com>
2023-12-04 15:41:22 +0900

    feat: #46 タブナビゲーションのデザインを改善

--- frontend/src/components/editing/EditingEntrysheets.tsx
+++ frontend/src/components/editing/EditingEntrysheets.tsx
@@ -14,29 +14,29 @@ const EditingEntrysheets = (props: {
   const handleChange = (selectedKey: string) => setSelectedKey(selectedKey);
 
   return (
-    <div className="flex flex-col p-2">
-      <label htmlFor="tabs" className="sr-only"></label>
-      <select
-        id="tabs"
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm
-              rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-              dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        onChange={(e) => handleChange(e.target.value)}
-      >
+    <div className="flex flex-col pl-2 pr-2">
+      {/* タブの選択 */}
+      <div className="flex">
         {Object.keys(editingEntrysheets).map((key) => (
-          <option key={key} value={key}>
+          <div
+            key={key}
+            className={`flex-shrink-0 px-4 py-2 cursor-pointer ${
+              key === selectedKey
+                ? "bg-white border-t border-l border-r border-gray-300 rounded-t-xl"
+                : "bg-gray-100 hover:bg-gray-200 rounded-t-xl"
+            }`}
+            onClick={() => handleChange(key)}
+          >
             {editingEntrysheets[Number(key)].company}
-          </option>
+          </div>
         ))}
-      </select>
+      </div>
 
       {/* 選択されたキーに対応するエントリーシートを表示 */}
       <div>
         {Object.keys(editingEntrysheets).map((key) => (
           <div key={key} className={key === selectedKey ? "block" : "hidden"}>
             <EditingEntrysheet
-              key={key}
               entrysheet={editingEntrysheets[Number(key)]}
               setEditingEntrysheets={setEditingEntrysheets}
             />
--- frontend/src/pages/users/[userId]/entrysheets.tsx
+++ frontend/src/pages/users/[userId]/entrysheets.tsx
@@ -50,7 +50,7 @@ const Entrysheets = ({ query }: GetServerSidePropsContext) => {
   if (!localEntrysheets || !UserIdContext) return <div>Loading...</div>;
 
   return (
-    <div className="flex p-4">
+    <div className="flex pl-4 pr-4">
       <UserIdContext.Provider value={query.userId}>
         <Sidebar
           entrysheets={localEntrysheets}
