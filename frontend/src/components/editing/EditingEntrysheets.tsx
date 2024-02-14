a2d6c4fda0d3e193e2e2d14a803410507d9f93be
tomishima <pumoco2904@icloud.com>
2024-02-14 17:59:39 +0900

    feat: #52 `Tab`コンポーネントを切り出し

--- frontend/src/components/editing/EditingEntrysheets.tsx
+++ frontend/src/components/editing/EditingEntrysheets.tsx
@@ -30,6 +30,32 @@ const EditingEntrysheets = (props: {
 
   const handleChange = (selectedTab: string) => setSelectedTab(selectedTab);
 
+  // `order`で selectedTab であるかそうでないかを判断して, スタイルを変える
+  const Tab = (props: { order: string }): JSX.Element => {
+    const { order } = props;
+    return (
+      <div
+        className={`flex-grow max-w-xs flex-shrink-0 px-4 py-2 text-sm cursor-pointer ${
+          order === selectedTab
+            ? "bg-white border-t border-l border-r border-gray-300 rounded-t-xl"
+            : "bg-gray-100 hover:bg-gray-200"
+        }`}
+        onClick={() => handleChange(order)}
+      >
+        <div className="flex justify-between">
+          <div className="">{entrysheets[Number(order)].company}</div>
+          <div className="flex-none">
+            <TabCloseButton
+              esId={Number(order)}
+              setEditingEntrysheets={setEditingEntrysheets}
+              setTabOrder={setTabOrder}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="flex flex-grow flex-shrink-0 flex-col pl-2 pr-2 ml-16">
       {/* タブの選択 */}
